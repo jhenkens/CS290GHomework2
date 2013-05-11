ab5815bd023b07e8324804486b9ee4b6df2c6f55
Johan Henkens <johan@henkens.com>
2013-05-11 15:28:42 -0700

    Initial commit

---
+++ CS290GHomework2/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  CS290GHomework2
+//
+//  Created by Johan Henkens on 5/11/13.
+//  Copyright (c) 2013 Johan Henkens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CS290GECC/CS290GECC.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        [CSLog log];
+        
+    }
+    return 0;
+}
+
